790707ccf3a159cbfb52548a645e28dbe408141e
Daniel Webster <danielwebsterps2@gmail.com>
2024-02-03 19:19:24 -0500

    fix dependency resolution

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
 export { thunderClient } from './thunder-client';
-
-import { thunderClient } from './thunder-client';
-
-thunderClient().then(async (client) => {
-  const state = await client.getState();
-  console.log(state);
-});
